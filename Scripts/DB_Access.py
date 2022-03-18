45d1a2b4ef025ded8e0bce68913cf59aba7da1ff
John Wiens <jwiens3141@gmail.com>
2022-03-17 21:50:20 -0600

    Added Future Event Predictions

--- Scripts/DB_Access.py
+++ Scripts/DB_Access.py
@@ -5,33 +5,33 @@ from datetime import datetime
 client = pymongo.MongoClient(config.DB_URI)
 
 
-def log_msg(msg, type = 'INFO',):
+def log_msg(msg, type = 'INFO', database = 'pf-database-2022'):
     event = {
         'DateTime' : datetime.now(),
         'Message': msg,
         'Type': type
     }
     print("DBLog", event)
-    insert_one('Log', event)
+    insert_one('Log', event, database=database)
     
 
-def insert_one(col, item):
-    db = client['pf-database-2022']
+def insert_one(col, item, database = 'pf-database-2022'):
+    db = client[database]
     col = db[col]
     return col.insert_one(item)
 
 
-def update_one(col, item):
-    db = client['pf-database-2022']
+def update_one(col, item, database = 'pf-database-2022'):
+    db = client[database]
     col = db[col]
     return col.replace_one({'key': item['key']}, item, upsert=True)
 
-def find_one(col, filter):
-    db = client['pf-database-2022']
+def find_one(col, filter, database = 'pf-database-2022'):
+    db = client[database]
     col = db[col]
     return col.find_one({'key':str(filter)})
 
-def find(col, filter):
-    db = client['pf-database-2022']
+def find(col, filter, database = 'pf-database-2022'):
+    db = client[database]
     col = db[col]
     return col.find(filter)
--- Scripts/Update_Data.py
+++ Scripts/Update_Data.py
@@ -28,7 +28,9 @@ def update_events(force_update = False):
             start = get_as_date(event['start_date'])
             end = get_as_date(event['end_date'])
             
-            if today >= start and today <= end or force_update: # code event['event_code'] == 'week0': #
+            if event['event_code'] == 'code':
+            #if today >= start:
+            #if today >= start and today <= end or force_update: # code event['event_code'] == 'week0': #
                 event_list.append(event['key'])
                 db.update_one('events', event)
 
@@ -62,12 +64,15 @@ def update_teams(event_code, force_update = False):
     teams, is_updated = TBA.get("/event/"+event_code+"/teams")
     db_teams = []
     for team in teams:
-        db_team = build_team(team) # Force the pydantic type for type checking
-        if db_team is not None:
-            as_dict = db_team.dict()
-            db.update_one('teams', as_dict)
-            db_teams.append(as_dict)
-
+        team_lookup = db.find_one('teams', team["key"][3:])
+        if force_update or team_lookup is None or team_lookup['opr'] == 0:
+            db_team = build_team(team) # Force the pydantic type for type checking
+            if db_team is not None:
+                as_dict = db_team.dict()
+                db.update_one('teams', as_dict)
+                db_teams.append(as_dict)
+        else:
+            db_teams.append(team_lookup)
     db_teams = sorted(db_teams, key=lambda k: float(k['key']))
 
     event = db.find_one('events',event_code)
@@ -77,14 +82,16 @@ def update_teams(event_code, force_update = False):
     return db_teams
 
 
-def update_calculations(event_code, matches, teams, force_update = False):
+def update_calculations(event_code, matches, teams, opr_coeffecients, force_update = False, ):
     rankings, is_updated = TBA.get("/event/"+event_code+"/rankings")
     team_list = [float(team['key']) for team in teams]
     team_array, score_array = build_score_matrix(event_code, team_list, matches)
     labeled_metrics = get_labeled_metrics(team_list, matches)
+    event_started = True
     if np.count_nonzero(score_array)==0:
-        db.log_msg(f"Data Array is Empty for {event_code}. Exiting")
-        return
+        event_started = False
+    #    db.log_msg(f"Data Array is Empty for {event_code}. Exiting")
+    #    return
     
     #team_powers = IRLS(score_array, team_array,1)
     team_powers = solve_matrix(team_array,score_array)
@@ -93,18 +100,43 @@ def update_calculations(event_code, matches, teams, force_update = False):
     estimator_error = np.square(score_array - estimator_scores) # Compute Squared Errors
     team_variances = solve_matrix(team_array, estimator_error) # Compute Each Teams contribution to squared error
     
+    # Fill in Missing Data as best as possible
+    skip_update = set()
+    for team in zip(teams, team_powers, labeled_metrics):
+        #print(team)
+        # No Data for this team at this event yet
+        if np.count_nonzero(team[2]) == 0 and np.count_nonzero(team[1]) == 0:
+            
+            #Check if they have played a previous event
+            previous_event_entry = db.find_one('teams', team[0]['key'])
+            if previous_event_entry is not None and previous_event_entry['opr'] is not None:
+                print('Loading from Previous Event', team[0]['key'])
+                team[1][1] = previous_event_entry['auto_pr'] / 4.0
+                team[1][3] = previous_event_entry['cargo_pr'] / 2.0
+                team[2][0] = previous_event_entry['climb_pr']
+                team[2][1] = previous_event_entry['taxi_pr']
+                skip_update.add(team[0]['key'])
+            else:
+                print('Loading from Archive', team[0]['key'])
+                archive_entry = db.find_one('Archive', team[0]['key'], database = 'pf-database')
+                if archive_entry is not None and archive_entry['opr'] is not None:
+                    normalized_opr = (clean_num(archive_entry['opr']) * 0.75)
+                    normalized_stats = map_opr(normalized_opr, opr_coeffecients)
+                    team[2][0] = normalized_stats[0] # Taxi
+                    team[1][1] = normalized_stats[2] / 4.0 # Auto
+                    team[1][3] = normalized_stats[1] / 2.0 # Cargo                    
+                    team[2][1] = normalized_stats[3] # Climb
     
+                
 
-    #team_powers = np.matmul(np.linalg.pinv(team_array),score_array)
     team_powers[np.isnan(team_powers)] = 0
     team_powers = np.around(team_powers,decimals = 2)
 
     team_variances[np.isnan(team_variances)] = 0
-    #team_variances = np.around(team_variances,decimals = 2)
+
     index = 0
-    #opr_matrix = team_powers[:,0] + team_powers[:,1] + endgame_array[:,0] + team_powers[:,3]
-    #variance_matrix = team_variances[:,0] + team_variances[:,1] + endgame_array[:,3] + team_variances[:,3]
-    #cell_variance = team_variances[:,4] + team_variances[:,5] + team_variances[:,6]
+
+
     opr_matrix = team_powers[:,0] * 2 + team_powers[:,1] * 4 + team_powers[:,2] + team_powers[:,3] * 2 + labeled_metrics[:,0] + labeled_metrics[:,1]
     max_opr = np.max(opr_matrix)
     for team in teams:
@@ -112,6 +144,7 @@ def update_calculations(event_code, matches, teams, force_update = False):
         pr = 100 * (opr/max_opr)
 
         try:
+            
             team['power'] = pr
             team['opr'] = opr
             team['opr_var'] = 0
@@ -127,35 +160,56 @@ def update_calculations(event_code, matches, teams, force_update = False):
             team['climb_pr_var'] = labeled_metrics[index,2]
             team['fouls'] = team_powers[index,4]
             index +=1
-            db.update_one('teams', team)
+            
+            if team['key'] not in skip_update:
+                print('Updating Team', team['key'])
+                db.update_one('teams', team)
             
         except Exception as e:
-            db.log_msg("Issue Updating Team Power Rankings"+ team+ str(e))
+            db.log_msg("Issue Updating Team Power Rankings"+ str(team)+ str(e))
         
     
     # Update Rankings Table
     try:
         table = []
         teams = np.array(team_list)
-        for ranking in rankings["rankings"]:
-
-            team = float((ranking["team_key"])[3:])
-            index = np.searchsorted(teams, team)
-            opr = opr_matrix[index]
-            
-            pr = 100 * (opr/max_opr)
-            rank = float(ranking["rank"])
-            row = {
-                "team": team,
-                "rank": rank,
-                "opr": opr,
-                "auto":team_powers[index,0] * 2 + team_powers[index,1] * 4 + labeled_metrics[index,1],
-                "climb":labeled_metrics[index,0],
-                "cargo": team_powers[index,2] + team_powers[index,3] * 2,
-                "cargo_count":team_powers[index,0] + team_powers[index,1] + team_powers[index,2] + team_powers[index,3],
-                "fouls":team_powers[index,4],
-                "power":pr}
-            table.append(Rank(**row))
+        if rankings is not None:
+            for ranking in rankings["rankings"]:
+                team = float((ranking["team_key"])[3:])
+                index = np.searchsorted(teams, team)
+                opr = opr_matrix[index]
+                
+                pr = 100 * (opr/max_opr)
+                rank = float(ranking["rank"])
+                row = {
+                    "team": team,
+                    "rank": rank,
+                    "opr": opr,
+                    "auto":team_powers[index,0] * 2 + team_powers[index,1] * 4 + labeled_metrics[index,1],
+                    "climb":labeled_metrics[index,0],
+                    "cargo": team_powers[index,2] + team_powers[index,3] * 2,
+                    "cargo_count":team_powers[index,0] + team_powers[index,1] + team_powers[index,2] + team_powers[index,3],
+                    "fouls":team_powers[index,4],
+                    "power":pr}
+                table.append(Rank(**row))
+        else:
+            for team in teams:
+                index = np.searchsorted(teams, team)
+                opr = opr_matrix[index]
+                
+                pr = 100 * (opr/max_opr)
+                row = {
+                    "team": team,
+                    "rank": 0,
+                    "opr": opr,
+                    "auto":team_powers[index,0] * 2 + team_powers[index,1] * 4 + labeled_metrics[index,1],
+                    "climb":labeled_metrics[index,0],
+                    "cargo": team_powers[index,2] + team_powers[index,3] * 2,
+                    "cargo_count":team_powers[index,0] + team_powers[index,1] + team_powers[index,2] + team_powers[index,3],
+                    "fouls":team_powers[index,4],
+                    "power":pr
+                }
+                table.append(Rank(**row))
         rankings = Rankings(**{'key': event_code, 'rankings':table})
         db.update_one('rankings', rankings.dict())                          
             
@@ -167,7 +221,7 @@ def clean_num(num):
     if num is None:
         return 0
     else:
-        return num
+        return float(num)
 
 
 
@@ -283,19 +337,62 @@ def update_rank_predictions(matches, teams):
     else:
         pass
 
+def update_opr_distribution_mapping():
+    data = db.find('teams',{})
+    if data is not None:
+        teams = list(data)
+        use_teams = []
+
+
+        for team in teams:
+            if team['opr_var'] is not None:
+                use_teams.append(team)
+
+        opr = np.zeros([len(use_teams),1])
+        metrics = np.zeros([len(use_teams),4])
+
+        for i, team in enumerate(use_teams):
+            
+            opr[i] = team['opr']
+            metrics[i][0] = team['taxi_pr']
+            metrics[i][1] = team['auto_pr']
+            metrics[i][2] = team['cargo_pr']
+            metrics[i][3] = team['climb_pr']
+            #print(team, metrics[i])
+        
+
+        #print(metrics)
+        coeffecients = np.linalg.pinv(opr) @ metrics
+        return coeffecients[0]
+
+def map_opr(opr, coeffecients):
+    scores = opr * coeffecients
+    if scores[0] > 2:
+        scores[2] += scores[0] - 2
+        scores[0] = 2
+    if scores[3] > 15:
+        scores[2] += scores[3] - 15
+        scores[3] = 15
+      
+    return scores
+        
+
+
 
 def update_data():
     if(TBA.check_connection()):
         events = update_events(force_update = False)
+        opr_coeffecients = update_opr_distribution_mapping()
+
         for event in events:
             matches = update_matches(event)
             teams = update_teams(event)
-            update_calculations(event, matches, teams)
+            update_calculations(event, matches, teams, opr_coeffecients)
             update_match_predictions(event, matches, teams)
             #update_rank_predictions(matches, teams)
         #    print(matches)
 
 if __name__ == '__main__':
     update_data()
-
+    #print(db.find_one('Archive', '1410', database = 'pf-database'))
         
