f2eb05a4eba3b9a28df2abe67fdf0a90ab3c44aa
John Wiens <jwiens3141@gmail.com>
2022-03-23 23:09:54 -0600

    fixed db url syntax

--- Scripts/config.py
+++ Scripts/config.py
@@ -3,5 +3,5 @@
 
 
 #DB_URI = "mongodb://root:example@0.0.0.0:27017/"
-DB_URI = #"mongodb://polar-forecast:dO8rkOkUTtob4zOHvdZYGmob0Jv1JQJMimetLfjxD9s8GPPFTAGc23oBYsbryiJi80Vhl7m6OfN5BOzlRtiKRQ==@polar-forecast.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@polar-forecast@"
+#DB_URI = "mongodb://polar-forecast:dO8rkOkUTtob4zOHvdZYGmob0Jv1JQJMimetLfjxD9s8GPPFTAGc23oBYsbryiJi80Vhl7m6OfN5BOzlRtiKRQ==@polar-forecast.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@polar-forecast@"
 DB_URI = "mongodb://polar-forecast-2022:DN06m4MAcv3zE8gdr2zHn9Bco4z1qJd4g3U8p1iM1zgFHpA7AWhjnF2Yun9WoOPeFvRDimdczwsO2pXK4yqw1w==@polar-forecast-2022.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@polar-forecast-2022@"
